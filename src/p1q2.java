0b8ded60bd9c30df5a8bf3631bcea456dbc845c9
Alex-Arthur Heard Williams <alexarthur.williams@gmail.com>
2017-02-06 11:37:18 -0800

    Add files via upload

--- src/p1q2.java
+++ src/p1q2.java
@@ -9,9 +9,9 @@ public class p1q2 {
       
       OperandStack operandStack = new OperandStack(100, "OperandStack Test");
 
-	 // if we have an expression line 54 + 32 we can translate that into postfix 
-      // like this: 54 32 +
-      // so it becomes: push(54), push (32), pop() twice and add and push the result.
+	// if we have an expression line 54 + 32 we can translate that into postfix 
+    // like this: 54 32 +
+    // so it becomes: push(54), push (32), pop() twice and add and push the result.
 	operandStack.push(new IntegerValue(54));
     operandStack.push(new IntegerValue(32));
 	
@@ -21,19 +21,45 @@ public class p1q2 {
 	v2 = operandStack.pop();
     v1 = operandStack.pop();
 	
-	   // Note, since the stack holds 'Value' values we have to cast the 
-      // values popped to the right kind.
+	// Note, since the stack holds 'Value' values we have to cast the 
+	// values popped to the right kind.
     IntegerValue v3, v4;
     v4 = (IntegerValue)v2;
 	v3 = (IntegerValue)v1;
 	
 	
 	
-  operandStack.push(new IntegerValue(v4.getValue() + v3.getValue())); //==> 86
-        System.out.println("peeking: " +operandStack.peek(1)); //==> Hello
+	operandStack.push(new IntegerValue(v4.getValue() + v3.getValue())); //==> 86
+    System.out.println("peeking: " +operandStack.peek(1)); //==> 89
 
-
+	operandStack.push(new IntegerValue(2));
 	
-  }
+	v2 = operandStack.pop();
+    v1 = operandStack.pop();
+
+    v4 = (IntegerValue)v2;
+	v3 = (IntegerValue)v1;	
+	
+	operandStack.push(new IntegerValue(v4.getValue() / v3.getValue()));
+	
+	operandStack.push(new IntegerValue(32));
+	operandStack.push(new IntegerValue(7));
+
+	v2 = operandStack.pop();	//7
+    v1 = operandStack.pop();	//32
+
+    v4 = (IntegerValue)v2;
+	v3 = (IntegerValue)v1;	
+	
+	operandStack.push(new IntegerValue(v3.getValue() - v4.getValue()));
+	
+	v2 = operandStack.pop();	//25 = v2
+    v1 = operandStack.pop();	//43 = v1
+
+    v4 = (IntegerValue)v2;
+	v3 = (IntegerValue)v1;	
+ 	operandStack.push(new IntegerValue(v3.getValue() * v4.getValue()));
+	System.out.println("Answer: " +operandStack.peek(1));
+ }
 
 }
